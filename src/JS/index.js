a6b8fe28fbb25f18b3c834377a638d2952d38f23
AmaJuli <ostapchukyuliiaa@gmail.com>
2023-06-25 15:43:38 +0300

    add

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,124 +1,87 @@
+const API_KEY = '37656081-35581442b65f56178bca80058'; 
 
-import {Notify} from 'notiflix/build/notiflix-notify-aio';
-import {findImages} from './images';
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
-const galleryEl = document.querySelector ('.gallery');
-const fromEl = document.querySelector ('#search-form');
-const buttonEl = document.querySelector ('.load-more');
-const inputEl = document.querySelector ('input');
-let nameImages = '';
-let page = 0;
-let perPage = 40;
-fromEl.addEventListener ('submit', create);
-buttonEl.addEventListener ('click', loadMore);
+let currentPage = 1;
+let currentQuery = '';
 
-function create (e) {
-  nameImages = '';
-  page = 0;
-  galleryEl.innerHTML = '';
-  buttonEl.classList.remove ('block');
-  e.preventDefault ();
-  if ((e.action = inputEl.value === '')) {
-    return Notify.failure (
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  }
-  nameImages = e.action = inputEl.value;
-  page += 1;
-  findImages (nameImages, perPage, page)
-    .then (res => {
-      if (!res.data.hits.length > 0) {
-        Notify.failure (
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  currentPage = 1;
+  currentQuery = e.target.elements.searchQuery.value.trim();
+  clearGallery();
+  searchImages(currentQuery, currentPage);
+});
+
+loadMoreBtn.addEventListener('click', () => {
+  currentPage++;
+  searchImages(currentQuery, currentPage);
+});
+
+function searchImages(query, page) {
+  const url = `https://pixabay.com/api/?key=${'37656081-35581442b65f56178bca80058'}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+
+  axios.get(url)
+    .then(response => {
+      const { data } = response;
+      const { hits, totalHits } = data;
+
+      if (hits.length > 0) {
+        displayImages(hits);
+        if (hits.length + (page - 1) * 40 < totalHits) {
+          showLoadMoreButton();
+        } else {
+          hideLoadMoreButton();
+        }
       } else {
-        return createCards (res), buttonEl.classList.add (
-          'block'
-        ), (buttonEl.disabled = false);
+        showNoImagesMessage();
       }
     })
-    .catch (error => {
-      Notify.failure (
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
+    .catch(error => {
+      console.error('Error:', error);
+      showErrorMessage();
     });
 }
 
-function loadMore (e) {
-  e.preventDefault ();
-  page += 1;
-  findImages (nameImages, perPage, page)
-    .then (res => {
-      if (!res.data.hits.length > 0) {
-        Notify.failure (
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        return createCards (res), hidingBtnLoadMore (res.data.totalHits);
-      }
-    })
-    .catch (error => {
-      Notify.failure (
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    });
+function displayImages(images) {
+  const imageCards = images.map(image => createImageCard(image));
+  gallery.innerHTML += imageCards.join('');
 }
-function createCards (arr) {
-  const object = arr.data.hits;
-  const card = object
-    .map (
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return ` <div class="photo-card">
-   <a class="gallery-link" href="${largeImageURL}">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" width = "300" height="250"/>
-    </a>
-    <div class="info">
-    <p class="info-item">
-    <b>Likes</b>
-    ${likes}
-    </p>
-    <p class="info-item">
-    <b>Views</b>
-    ${views}
-    </p>
-    <p class="info-item">
-    <b>Comments</b>
-    ${comments}
-    </p>
-    <p class="info-item">
-    <b>Downloads</b>
-    ${downloads}
-    </p>
+
+function createImageCard(image) {
+  return `
+    <div class="photo-card">
+      <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+        <p class="info-item"><b>Views:</b> ${image.views}</p>
+        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+      </div>
     </div>
-    </div> `;
-      }
-    )
-    .join ('');
-  return galleryEl.insertAdjacentHTML (
-    'beforeend',
-    card
-  ), new SimpleLightbox ('.gallery a', {
-    captionsData: 'alt',
-    captionsDelay: 250,
-    disableScroll: false,
-  }).refresh ();
+  `;
 }
 
-function hidingBtnLoadMore (total) {
-  let comparison = page * perPage < total;
-  if (!comparison) {
-    (buttonEl.disabled = true), Notify.failure (
-      "We're sorry, but you've reached the end of search results."
-    );
-  } else {
-    return;
-  }
+function clearGallery() {
+  gallery.innerHTML = '';
 }
+
+function showLoadMoreButton() {
+  loadMoreBtn.style.display = 'block';
+}
+
+function hideLoadMoreButton() {
+  loadMoreBtn.style.display = 'none';
+}
+
+function showNoImagesMessage() {
+  Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+}
+
+function showErrorMessage() {
+  Notiflix.Notify.failure('An error occurred while fetching images. Please try again later.');
+}
+
+
